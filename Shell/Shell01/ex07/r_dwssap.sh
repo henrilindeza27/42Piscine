e85f6a327dfc5f7924887012101ae6a1c177c083
henrilindeza27@gmail.com <henrilindeza27@gmail.com>
2023-02-20 11:55:28 +0000

    Shell00 && Shell01

---
+++ Shell/Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -v '^#' |awk -F: '{print $1}'| sed -n '2~2p' | rev | sort -r | awk -v line1="$FT_LINE1" -v line2="$FT_LINE2" 'NR>= line1 && NR<=line2 || NR>=line2 && NR<=line1' | tr '\n' ' ' | sed 's/ /, /g' |tr -d "\n" | sed 's/, $/./'
